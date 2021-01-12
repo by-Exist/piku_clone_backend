e35da3bafb1d674b658d8d29e4bc468588e8bc24
Exist <bolk9652@naver.com>
2021-01-12 18:46:57 +0900

    📄docs: add django-environ-docker, edit settings

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -1,1 +1,29 @@
 from .common import *
+import environ
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+
+DEBUG = True
+
+# https://pypi.org/project/django-environ-docker/
+env = environ.Env()
+env.read_env()
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env("DJANGO_SECRET_KEY")
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+# MEDIA_ROOT
+# https://docs.djangoproject.com/en/3.1/ref/settings/#std:setting-MEDIA_ROOT
+MEDIA_ROOT = BASE_DIR / "backend" / "media"  # use dev upload file path
--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -1,1 +1,21 @@
 from .common import *
+import environ
+
+DEBUG = False
+
+# https://pypi.org/project/django-environ-docker/
+env = environ.Env()
+env.read_docker_secrets()
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env("DJANGO_SECRET_KEY")
+
+ALLOWED_HOSTS = []  # FIXME: Set ALLOWED_HOSTS, ex) env.list("ALLOWED_HOSTS")
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+DATABASES = {}  # FIXME: Set DATABASES
+
+# Logging
+# https://docs.djangoproject.com/en/3.1/topics/logging/
+LOGGING = {}  # FIXME: Set LOGGING
